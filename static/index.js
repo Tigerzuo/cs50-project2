dcfc00e5716a1c3453e3cc7a842ba5c0d601b462
Tiger <tiger@Tigers-MacBook-Air.local>
2020-07-01 15:07:15 -0700

    fix index.js src

--- static/index.js
+++ static/index.js
@@ -1,15 +1,13 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('connect', function () {
-    socket.emit('my event', {
-        data: 'User Connected'
-    })
-    var form = $('form').on('submit', function (e) {
+    var form = $('form.new_message').on('submit', function (e) {
         e.preventDefault()
-        let user_name = $('input.username').val()
         let user_input = $('input.message').val()
         socket.emit('my event', {
-            user_name: user_name,
+            message: user_input
+        })
+        socket.emit('new message', {
             message: user_input
         })
         $('input.message').val('').focus()
@@ -17,8 +15,6 @@ socket.on('connect', function () {
 })
 socket.on('my response', function (msg) {
     console.log(msg)
-    if (typeof msg.user_name !== 'undefined') {
-        $('h3').remove()
-        $('div.message_holder').append('<div><b style="color: #000">' + msg.user_name + '</b> ' + msg.message + '</div>')
-    }
+    $('h3').remove()
+    $('div.message_holder').append('<div><b style="color: #000">' + msg.username + ':' + '</b> ' + msg.message + '</div>')
 })
